0c777a64365e0e203957e16b2d9bd6bcfda7f6b0
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-03 16:54:58 +0800

    add test for parkingBoy to park in first carpark

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -2,6 +2,9 @@ package com.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class ParkingBoyTest {
@@ -18,5 +21,22 @@ public class ParkingBoyTest {
 
         // then
         assertNotNull(ticket);
+    }
+
+    @Test
+    public void should_park_to_first_car_park_when_parkCar_given_valid_car_and_first_car_park_not_full_capacity() throws Exception{
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(List.of(parkingLot1, parkingLot2));
+
+        // when
+        Ticket ticket = parkingBoy.parkCar(car);
+
+        // then
+        assertNotNull(ticket);
+        assertEquals(9, parkingLot1.getCurrentCapacity());
+        assertEquals(10, parkingLot1.getCurrentCapacity());
     }
 }
