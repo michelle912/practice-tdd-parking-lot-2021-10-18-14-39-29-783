83efd6bbfae4b53193f2fad23d40d4552974e7a5
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-03 17:30:41 +0800

    add test for parkingBoy to park in second carpark

--- src/test/java/com/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/parkinglot/ParkingBoyTest.java
@@ -40,4 +40,23 @@ public class ParkingBoyTest {
         assertEquals(9, parkingLot1.getCurrentCapacity());
         assertEquals(10, parkingLot2.getCurrentCapacity());
     }
+
+    @Test
+    public void should_park_to_second_car_park_when_parkCar_given_valid_car_and_first_car_park_full_capacity() throws Exception{
+        // given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        parkingLot1.parkCar(car1);
+        ParkingLot parkingLot2 = new ParkingLot();
+        ParkingBoy parkingBoy = new ParkingBoy(List.of(parkingLot1, parkingLot2));
+
+        // when
+        Ticket ticket = parkingBoy.parkCar(car2);
+
+        // then
+        assertNotNull(ticket);
+        assertEquals(0, parkingLot1.getCurrentCapacity());
+        assertEquals(9, parkingLot2.getCurrentCapacity());
+    }
 }
