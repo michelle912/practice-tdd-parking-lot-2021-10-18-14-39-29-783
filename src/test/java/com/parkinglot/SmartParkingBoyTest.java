a3ed6fc0f069bfa19b787c5e60cd4ecbbebeabd5
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-04 21:22:01 +0800

    add test for smartParkingBoy to throw exception :

--- src/test/java/com/parkinglot/SmartParkingBoyTest.java
+++ src/test/java/com/parkinglot/SmartParkingBoyTest.java
@@ -64,4 +64,52 @@ public class SmartParkingBoyTest {
         assertEquals(car2, result2);
     }
 
+    @Test
+    public void should_throw_exception_when_fetchCar_given_unrecognized_ticket() throws Exception{
+        // given
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(List.of(parkingLot1, parkingLot2));
+        Ticket invalidTicket = new Ticket();
+
+        // when
+
+        // then
+        assertThrows(UnrecognizedTicketException.class, () -> smartParkingBoy.fetchCar(invalidTicket));
+    }
+
+    @Test
+    public void should_throw_exception_when_fetchCar_given_used_ticket() throws Exception{
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(List.of(parkingLot1, parkingLot2));
+        Ticket ticket = smartParkingBoy.parkCar(car);
+        smartParkingBoy.fetchCar(ticket);
+
+        // when
+
+        // then
+        assertThrows(UnrecognizedTicketException.class, () -> smartParkingBoy.fetchCar(ticket));
+    }
+
+    @Test
+    public void should_throw_exception_when_fetchCar_given_all_full_capacity() throws Exception{
+        // given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot(1);
+        SmartParkingBoy smartParkingBoy = new SmartParkingBoy(List.of(parkingLot1, parkingLot2));
+        smartParkingBoy.parkCar(car1);
+        smartParkingBoy.parkCar(car2);
+
+        // when
+
+        // then
+        assertThrows(NoAvailablePositionException.class, () -> smartParkingBoy.parkCar(car3));
+    }
+
 }
