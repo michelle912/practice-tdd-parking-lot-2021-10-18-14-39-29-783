8d085becc6fdfad238b5c3c74e6dd69219beaf0e
michelle <81959777+michelle912@users.noreply.github.com>
2021-12-05 23:15:40 +0800

    add test case for parkingLotManager

--- src/main/java/com/parkinglot/ParkingLotManager.java
+++ src/main/java/com/parkinglot/ParkingLotManager.java
@@ -1,5 +1,36 @@
 package com.parkinglot;
 
-public class ParkingLotManager {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ParkingLotManager implements ParkingBoy{
+    private List<ParkingBoy> parkingBoyList;
+    private List<ParkingLot> parkingLotList;
+
+    public ParkingLotManager() {
+        parkingBoyList = new ArrayList<>();
+        parkingLotList = new ArrayList<>();
+    }
+
+    public ParkingLotManager(List<ParkingBoy> parkingBoyList) {
+        this.parkingBoyList = parkingBoyList;
+    }
+
+    @Override
+    public Ticket parkCar(Car car) throws NoAvailablePositionException {
+        return null;
+    }
+
+    @Override
+    public Car fetchCar(Ticket ticket) throws UnrecognizedTicketException {
+        return null;
+    }
+
+    public Ticket parkCarByParkingBoy(Car car, ParkingBoy parkingBoy) {
+        return null;
+    }
+
+    public void setParkingLot(List<ParkingLot> parkingLotList) {
+        this.parkingLotList = parkingLotList;
+    }
 }
---
+++ src/test/java/com/parkinglot/ParkingLotManagerTest.java
@@ -0,0 +1,204 @@
+package com.parkinglot;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParkingLotManagerTest {
+
+    @Disabled
+    @Test
+    public void should_add_to_manage_list_when_addManagementList_given_valid_parkingBoy() {
+        // given
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    public void should_park_car_in_parkingboy_car_park_when_parkCarByParkingBoy_given_valid_parkingBoy_valid_car_not_full_capacity() throws Exception {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        NormalParkingBoy normalParkingBoy = new NormalParkingBoy(List.of(parkingLot1, parkingLot2));
+        ParkingLotManager parkingLotManager = new ParkingLotManager(List.of(normalParkingBoy));
+
+        // when
+        Ticket ticket = parkingLotManager.parkCarByParkingBoy(car, normalParkingBoy);
+
+        // then
+        assertNotNull(ticket);
+        assertEquals(9, parkingLot1.getCurrentCapacity());
+        assertEquals(10, parkingLot2.getCurrentCapacity());
+
+    }
+
+
+    @Test
+    public void should_throw_exception_when_parkCarByParkingBoy_given_invalid_parkingBoy() {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        NormalParkingBoy normalParkingBoy = new NormalParkingBoy(Collections.emptyList());
+        ParkingLotManager parkingLotManager = new ParkingLotManager();
+
+        // when
+
+        // then
+        assertThrows(Exception.class, () -> parkingLotManager.parkCarByParkingBoy(car, normalParkingBoy)); // FIXME:
+
+    }
+
+    @Test
+    public void should_throw_exception_when_parkCarByParkingBoy_given_valid_parkingBoy_valid_car_full_capacity() {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        NormalParkingBoy normalParkingBoy = new NormalParkingBoy(List.of(parkingLot1, parkingLot2));
+        ParkingLotManager parkingLotManager = new ParkingLotManager(List.of(normalParkingBoy));
+
+        // when
+
+        // then
+        assertThrows(NoAvailablePositionException.class, () -> parkingLotManager.parkCarByParkingBoy(car, normalParkingBoy));
+
+    }
+
+    @Test
+    public void should_fetch_car_in_parkingboy_car_park_when_fetchCar_given_valid_ticket_and_car_exist_in_parkingboy_car_park() throws Exception {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        NormalParkingBoy normalParkingBoy = new NormalParkingBoy(List.of(parkingLot1, parkingLot2));
+        ParkingLotManager parkingLotManager = new ParkingLotManager(List.of(normalParkingBoy));
+        Ticket ticket = parkingLotManager.parkCarByParkingBoy(car, normalParkingBoy);
+
+        // when
+        Car result = parkingLotManager.fetchCar(ticket);
+
+        // then
+        assertNotNull(result);
+
+    }
+
+    @Test
+    public void should_throw_exception_when_fetchCar_given_invalid_ticket() throws Exception {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        NormalParkingBoy normalParkingBoy = new NormalParkingBoy(List.of(parkingLot1, parkingLot2));
+        ParkingLotManager parkingLotManager = new ParkingLotManager(List.of(normalParkingBoy));
+
+        // when
+        Ticket ticket = new Ticket();
+
+        // then
+        assertThrows(UnrecognizedTicketException.class, () -> parkingLotManager.fetchCar(ticket));
+
+    }
+
+    @Test
+    public void should_throw_exception_when_fetchCar_given_used_ticket() throws Exception {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        NormalParkingBoy normalParkingBoy = new NormalParkingBoy(List.of(parkingLot1, parkingLot2));
+        ParkingLotManager parkingLotManager = new ParkingLotManager(List.of(normalParkingBoy));
+        Ticket ticket = parkingLotManager.parkCarByParkingBoy(car, normalParkingBoy);
+        parkingLotManager.fetchCar(ticket);
+
+        // when
+
+        // then
+        assertThrows(UnrecognizedTicketException.class, () -> parkingLotManager.fetchCar(ticket));
+
+    }
+
+    @Test
+    public void should_park_car_in_own_car_park_when_parkCar_given_valid_car_not_full_capacity() throws Exception {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        ParkingLotManager parkingLotManager = new ParkingLotManager();
+        parkingLotManager.setParkingLot(List.of(parkingLot1, parkingLot2));
+
+        // when
+        Ticket ticket = parkingLotManager.parkCar(car);
+
+        // then
+        assertNotNull(ticket);
+        assertEquals(9, parkingLot1.getCurrentCapacity());
+        assertEquals(10, parkingLot2.getCurrentCapacity());
+
+    }
+
+    @Test
+    public void should_fetch_car_in_own_car_park_when_fetchCar_given_valid_ticket_and_car_exist() throws Exception {
+        // given
+        Car car = new Car();
+        ParkingLot parkingLot1 = new ParkingLot();
+        ParkingLot parkingLot2 = new ParkingLot();
+        ParkingLotManager parkingLotManager = new ParkingLotManager();
+        parkingLotManager.setParkingLot(List.of(parkingLot1, parkingLot2));
+        Ticket ticket = parkingLotManager.parkCar(car);
+
+        // when
+        Car result = parkingLotManager.fetchCar(ticket);
+
+        // then
+        assertEquals(car, result);
+
+    }
+
+    @Test
+    public void should_park_to_own_second_car_park_when_parkCar_given_valid_car_first_car_park_full_capacity() throws Exception {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLot parkingLot2 = new ParkingLot();
+        ParkingLotManager parkingLotManager = new ParkingLotManager();
+        parkingLotManager.setParkingLot(List.of(parkingLot1, parkingLot2));
+        parkingLotManager.parkCar(car1);
+        assertEquals(0, parkingLot1.getCurrentCapacity());
+        assertEquals(10, parkingLot1.getCurrentCapacity());
+
+        // when
+        Ticket ticket = parkingLotManager.parkCar(car2);
+
+        // then
+        assertNotNull(ticket);
+        assertEquals(9, parkingLot2.getCurrentCapacity());
+
+    }
+
+    @Test
+    public void should_throw_exception_when_parkCar_given_valid_car_full_capacity() throws Exception {
+        // given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        ParkingLot parkingLot1 = new ParkingLot(1);
+        ParkingLotManager parkingLotManager = new ParkingLotManager();
+        parkingLotManager.setParkingLot(List.of(parkingLot1));
+        parkingLotManager.parkCar(car1);
+
+        // when
+
+        // then
+        assertThrows(NoAvailablePositionException.class, () -> parkingLotManager.parkCar(car2));
+
+    }
+
+}
